8be8d9eb463f9bf80e62cee992981263e202df39
Hassan Kamal <hk977363@gmail.com>
2026-03-03 00:12:40 +0200

    edit time stamp2

--- act.php
+++ act.php
@@ -1,16 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
-            $Tittle=htmlspecialchars($_POST["Tittle"]);
-            $content=htmlspecialchars($_POST["content"]);
+            $Tittle=trim($_POST["Tittle"]);
+            $content=trim($_POST["content"]);
             if (empty($Tittle) || empty($content)) {
                 echo   "All fields are required.";
                 exit();
             }else{
                         $date = date("d M Y - h:i A");
                         $file = fopen("notes.txt", "a");
-                         fwrite($file, $Tittle . "|" . $content . "|" . $date . "\n");
-                         fclose($file);
-                             header("Location: notes.txt");
+                        fwrite($file, $Tittle . "|" . $content . "|" . $date . "\n");
+                        fclose($file);
+                        header("Location: index.php");
         exit();
             }
     
